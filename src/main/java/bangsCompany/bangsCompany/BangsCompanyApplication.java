75d212322700a15478aa79e2dd9692df9ccbc7e2
wangsungtaek <tjdxorv@naver.com>
2021-09-12 23:42:16 +0900

    first commit

---
+++ src/main/java/bangsCompany/bangsCompany/BangsCompanyApplication.java
@@ -0,0 +1,13 @@
+package bangsCompany.bangsCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangsCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangsCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bangsCompany/bangsCompany/BangsCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package bangsCompany.bangsCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangsCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
