ebf6c862ae274c4ed4d3c88006037ff99ac3c260
wangsungtaek <tjdxorv@naver.com>
2021-09-24 22:59:41 +0900

    정적 파일 참조

--- src/main/java/bangsCompany/bangsCompany/main/controller/MainController.java
+++ src/main/java/bangsCompany/bangsCompany/main/controller/MainController.java
@@ -1,14 +1,15 @@
-package bangsCompany.main.controller;
+package bangsCompany.bangsCompany.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-    @GetMapping("hello")
-    public String mainPage() {
+    @GetMapping("main")
+    public String mainPage(Model model) {
         System.out.println("11");
-        return "index";
+        return "main";
     }
 }
