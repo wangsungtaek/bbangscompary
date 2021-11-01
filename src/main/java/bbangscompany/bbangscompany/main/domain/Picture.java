56879e1a14b46edf3803c592803cef6aeeb66fff
wangsungtaek <tjdxorv@naver.com>
2021-11-01 23:54:37 +0900

    호스팅 테스트

---
+++ src/main/java/bbangscompany/bbangscompany/main/domain/Picture.java
@@ -0,0 +1,50 @@
+package bbangscompany.bbangscompany.main.domain;
+
+public class Picture {
+
+    private Long id;
+    private String name;
+    private String channelName;
+    private String imgPath;
+    private String link;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public void setChannelName(String channelName) {
+        this.channelName = channelName;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
