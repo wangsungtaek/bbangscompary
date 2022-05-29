456e4d2387d36751f1d3938763702d8b3b888867
wangsungtaek <tjdxorv@naver.com>
2022-05-29 22:46:49 +0900

    main화면 업데이트

--- src/main/java/bbangscompany/controller/MainController.java
+++ src/main/java/bbangscompany/controller/MainController.java
@@ -1,12 +1,10 @@
 package bbangscompany.controller;
 
+import bbangscompany.domain.Collaborate;
 import bbangscompany.domain.Image;
 import bbangscompany.domain.Picture;
 import bbangscompany.domain.ContactUs;
-import bbangscompany.service.ContactUsService;
-import bbangscompany.service.CoverImageService;
-import bbangscompany.service.ImageService;
-import bbangscompany.service.MainService;
+import bbangscompany.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,13 +22,16 @@ public class MainController {
     private final ContactUsService contactUsService;
     private final ImageService imageService;
     private final CoverImageService coverImageService;
+    private final CollaborateService collaborateService;
 
     @GetMapping("")
     public String savePicture(Model model) {
 
         List<Image> images = imageService.findImages();
+        List<Collaborate> collaborates = collaborateService.findCollaborates();
 
         model.addAttribute("coverImage", coverImageService.findOne(1L));
+        model.addAttribute("collaborates", collaborates);
         model.addAttribute("images", images);
 
         return "main";
